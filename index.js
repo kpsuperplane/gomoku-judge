f5147ce4b65aacd48b1c4769e501ddf2717da76a
Kevin Pei <kevin-pei@hotmail.com>
2016-10-30 23:20:13 -0400

    docs

--- index.js
+++ index.js
@@ -167,5 +167,5 @@ app.get('/move', function(req, res){
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Gomoku app listening on port 3000!');
 });
